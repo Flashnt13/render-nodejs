d9c0d1b5c798c70170ed033ba030f0da75972ccb
Luciano Aguilar <456luciano456@gmail.com>
2024-03-27 17:08:16 -0400

    Third commit - Postgress anadido

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import Express from "express";
+import pg from 'pg';
+import { config } from "dotenv";
+
+config();
 
 const app = Express();
-
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('Hello world');
+})
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
 })
 
 app.listen(3000);
 
-console.log('server on port', 3000)
+console.log('server on port', 3000);
